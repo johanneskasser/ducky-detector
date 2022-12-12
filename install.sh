5deab30c328f1bf13df938598e3f095e291733e2
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2022-12-12 12:21:36 +0100

    install.sh: Loading Indicator + some improvements

--- install.sh
+++ install.sh
@@ -7,14 +7,18 @@ if [ "$user" != "root" ]; then
   exit
 else
   echo "=== Installing Ducky Detector v2.1 ==="
+  echo "--- Installing required packages... (This may take a while) ---"
 
-  sudo apt-get update
-  sudo apt-get upgrade
+  loading 1
+  sudo apt-get update &>> ./debug.txt
+  sudo apt-get upgrade &>> ./debug.txt
 
-  sudo apt-get install g++ libusb-1.0-0-dev clamav libclamav-dev libgtkmm-3.0-dev
+  sudo apt-get --assume-yes install g++ libusb-1.0-0-dev clamav libclamav-dev libgtkmm-3.0-dev &>> ./debug.txt
 
   mkdir -p /mnt/mount
   sudo make
+
+  loading 0
 
   echo "=== Installation Complete. Do you want to configure the touchscreen display? ==="
   echo "(1) yes"
@@ -22,7 +26,9 @@ else
   read -rp "> " answerTouchscreen
 
   if [ "$answerTouchscreen" = 1 ]; then
+    loading 1
     echo "Haha not implemented yet"
+    loading 0
   else
     echo "OK, good choice"
   fi
@@ -38,5 +44,35 @@ else
     echo "=== Installation Complete. Starting ducky-detector! ==="
     sudo ./DuckyDetector
   fi
+
 fi
 
+function loading() {
+  printf "Loading, please wait.. "
+
+  sp="/-\|"
+  sc=0
+
+  working=$1
+
+
+  until [ "$working" -lt 1 ]
+  do
+     spin
+  done
+  endspin
+}
+
+sp="/-\|"
+sc=0
+
+function spin() {
+
+   printf "\b${sp:sc++:1}"
+   ((sc==${#sp})) && sc=0
+  }
+
+# shellcheck disable=SC2120
+function endspin() {
+    printf "\r%s\n" "$@"
+}
