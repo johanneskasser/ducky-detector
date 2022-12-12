5c17fde865358c6b3a8f7f8ff66b7d619d9a715c
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2022-12-12 11:26:01 +0100

    install.sh improvements

--- install.sh
+++ install.sh
@@ -1,24 +1,42 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-printf "=== Installing Ducky Detector v2.1 === \n"
-printf "--- Please make sure to enter super-user password to complete the installation --- \n"
+user=$(whoami)
 
-sudo apt-get update
-sudo apt-get upgrade
-
-sudo apt-get install g++ libusb-1.0-0-dev clamav libclamav-dev libgtkmm-3.0-dev
-
-mkdir -p /mnt/mount
-make
-
-printf "=== Installation Complete. Do you want to configure the touchscreen display? ===\n"
-printf "(1) yes\n"
-printf "(2) no\n"
-read -r answerTouchscreen
-
-if [ "$answerTouchscreen" = 1 ]; then
-  printf "Haha not implemented yet \n"
+if [ "$user" != "root" ]; then
+  echo "=== FATAL ERROR: You need to execute this script as root user! ==="
+  exit
 else
-  printf "OK, good choice \n"
+  echo "=== Installing Ducky Detector v2.1 ==="
+
+  sudo apt-get update
+  sudo apt-get upgrade
+
+  sudo apt-get install g++ libusb-1.0-0-dev clamav libclamav-dev libgtkmm-3.0-dev
+
+  mkdir -p /mnt/mount
+  sudo make
+
+  echo "=== Installation Complete. Do you want to configure the touchscreen display? ==="
+  echo "(1) yes"
+  echo "(2) no"
+  read -rp "> " answerTouchscreen
+
+  if [ "$answerTouchscreen" = 1 ]; then
+    echo "Haha not implemented yet"
+  else
+    echo "OK, good choice"
+  fi
+
+  echo "=== Do you want to start the ducky-detector everytime the raspberry pi is started? ==="
+  echo "(1) yes"
+  echo "(2) no"
+  read -rp "> " answerAutoStart
+
+  if [ "$answerAutoStart" = 1 ]; then
+    echo "Not Implemented yet"
+  else
+    echo "=== Installation Complete. Starting ducky-detector! ==="
+    sudo ./DuckyDetector
+  fi
 fi
 
