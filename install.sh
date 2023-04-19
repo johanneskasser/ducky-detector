c680133c6e70982966859a9d1880bd40a0077194
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2023-04-19 15:33:21 +0200

    New Install Script

--- install.sh
+++ install.sh
@@ -2,58 +2,44 @@
 
 total_steps=10
 
-bar_width=50
-
-bar_char="#"
-empty_char="-"
-
-function update_bar() {
-  # Calculate the percentage of completion
-  completed_steps=$1
-  percentage=$((completed_steps * 100 / total_steps))
-
-  # Calculate the number of characters to draw in the bar
-  bar_size=$((bar_width * completed_steps / total_steps))
-
-  # Draw the loading bar
-  bar=$(printf "[%-${bar_width}s]" "${bar_char:0:bar_size}")
-    empty=$(printf "[%-${bar_width}s]" "${empty_char:0:$((bar_width - bar_size))}")
-    printf "\033[2K\r%s %d%% %s" "$bar" "$percentage" "$empty"
+prog() {
+    local w=80 p=0;
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=\${dots// /.};
+    printf -v progress "|%-*s|" "$w" "$dots";
+    printf "\r\e[K%s" "$progress";
+    for i in $(seq 1 $total_steps); do
+        printf ".";
+    done
 }
 
-# Define the function to call when a step is completed
-function completedStep() {
-  # Increment the completed steps counter
-  completed_steps=$((completed_steps + 1))
-
-  # Clear the previous loading bar
-  printf "\033[2K\r"
-
-  # Update the loading bar
-  update_bar "$completed_steps"
-
-  # Check if all steps have been completed
-  if [ "$completed_steps" -eq "$total_steps" ]; then
-    echo ""
-    echo "Task completed!"
-  fi
+completedStep() {
+    p=$((p+1))
+    printf -v dots "%*s" "$(( $p*$w/100 ))" ""; dots=\${dots// /.};
+    printf -v progress "|%-*s|" "$w" "$dots";
+    printf "\r\e[K%s" "$progress";
+    for i in $(seq 1 $total_steps); do
+        if [ $i -le $p ]; then
+            printf ".";
+        else
+            printf " ";
+        fi
+    done
 }
+
 
 function installTouchScreen {
   echo "=== Configuration of Touchscreen ==="
 
-
-
   if [ -f "/boot/config.txt" ]; then
     echo "#Joy-IT Touchscreen configuration"
-    echo "dtparam=spi=on" >> /boot/config.txt
-    echo "dtoverlay=joy-IT-Display-Driver-32b-overlay:rotate=270,swapxy=1" >> /boot/config.txt
-    echo "dtparam=audio=on" >> /boot/config.txt
-    echo "max_usb_current=1" >> /boot/config.txt
-    echo "hdmi_force_hotplug=1" >> /boot/config.txt
-    echo "config_hdmi_boost=7" >> /boot/config.txt
-    echo "hdmi_drive=1" >> /boot/config.txt
-    echo "hdmi_ignore_edid=0xa5000080" >> /boot/config.txt
+    echo "dtparam=spi=on" >>/boot/config.txt
+    echo "dtoverlay=joy-IT-Display-Driver-32b-overlay:rotate=270,swapxy=1" >>/boot/config.txt
+    echo "dtparam=audio=on" >>/boot/config.txt
+    echo "max_usb_current=1" >>/boot/config.txt
+    echo "hdmi_force_hotplug=1" >>/boot/config.txt
+    echo "config_hdmi_boost=7" >>/boot/config.txt
+    echo "hdmi_drive=1" >>/boot/config.txt
+    echo "hdmi_ignore_edid=0xa5000080" >>/boot/config.txt
 
     sed /dtoverlay=vc4-fkms-v3d/d /boot/config.txt
   else
@@ -68,12 +54,12 @@ function installTouchScreen {
     return
   fi
 
-  echo "Section "InputClass"" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
-  echo   "Identifier "calibration"" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
-  echo   "MatchProduct "ADS7846 Touchscreen"" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
-  echo   "Option "Calibration" "160 3723 3896 181"" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
-  echo   "Option "SwapAxes" "0"" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
-  echo "EndSection" >> /usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "Section "InputClass"" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "Identifier "calibration"" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "MatchProduct "ADS7846 Touchscreen"" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "Option "Calibration" "160 3723 3896 181"" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "Option "SwapAxes" "0"" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
+  echo "EndSection" >>/usr/share/X11/xorg.conf.d/99-calibration.conf
 
 }
 
@@ -82,10 +68,10 @@ function enableAutoStartup {
 
   mkdir /home/pi/.config/autostart
 
-  echo "[Desktop Entry]" >> /home/pi/.config/autostart/duckyDetector.desktop
-  echo "Name=DuckyDetector" >> /home/pi/.config/autostart/duckyDetector.desktop
-  echo "Exec=sudo /home/pi/ducky-detector/DuckyDetector" >> /home/pi/.config/autostart/duckyDetector.desktop
-  echo "Type=Application" >> /home/pi/.config/autostart/duckyDetector.desktop
+  echo "[Desktop Entry]" >>/home/pi/.config/autostart/duckyDetector.desktop
+  echo "Name=DuckyDetector" >>/home/pi/.config/autostart/duckyDetector.desktop
+  echo "Exec=sudo /home/pi/ducky-detector/DuckyDetector" >>/home/pi/.config/autostart/duckyDetector.desktop
+  echo "Type=Application" >>/home/pi/.config/autostart/duckyDetector.desktop
 
   if [ -f "/home/pi/.config/autostart/duckyDetector.desktop" ]; then
     echo "Startup configuration successful!"
@@ -96,7 +82,7 @@ function enableAutoStartup {
 
 user=$(whoami)
 
-update_bar 0
+prog
 
 if [ "$user" != "root" ]; then
   echo "=== FATAL ERROR: You need to execute this script as root user! ==="
@@ -105,28 +91,24 @@ else
   echo "=== Installing Ducky Detector v2.1 ==="
   echo "--- Installing required packages (This may take a while)... ---"
 
-
-  sudo apt-get update &>> ./debug.txt
+  sudo apt-get update &>>./debug.txt
   completedStep
-  sudo apt-get upgrade &>> ./debug.txt
-  completedStep
-
-  sudo apt-get --assume-yes install g++ &>> ./debug.txt
-  completedStep
-  sudo apt-get --assume-yes install clamav &>> ./debug.txt
-  completedStep
-  sudo apt-get --assume-yes install libclamav-dev &>> ./debug.txt
-  completedStep
-  sudo apt-get --assume-yes install llibgtkmm-3.0-dev &>> ./debug.txt
+  sudo apt-get upgrade &>>./debug.txt
   completedStep
 
+  sudo apt-get --assume-yes install g++ &>>./debug.txt
+  completedStep
+  sudo apt-get --assume-yes install clamav &>>./debug.txt
+  completedStep
+  sudo apt-get --assume-yes install libclamav-dev &>>./debug.txt
+  completedStep
+  sudo apt-get --assume-yes install llibgtkmm-3.0-dev &>>./debug.txt
+  completedStep
 
   mkdir -p /mnt/mount
   completedStep
-  sudo make &>> ./debug.txt
+  sudo make &>>./debug.txt
   completedStep
-
-
 
   echo "=== Installation Complete. Do you want to configure the touchscreen display? ==="
   echo "(1) yes"
@@ -157,4 +139,3 @@ else
   fi
   completedStep
 fi
-
